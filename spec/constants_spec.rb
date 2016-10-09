d1188505c484007dc1a9db8aa523edc4fc079df9
Mateusz Sagan <m.sagan@binarapps.com>
2016-10-09 05:41:48 +0200

    passing tests for constant variables

--- spec/constants_spec.rb
+++ spec/constants_spec.rb
@@ -1,25 +1,23 @@
 describe Beheader::Constants do
-  let(:urL_characters) { Beheader::Constants::URL_CHARACTERS }
+  let(:url_characters) { Beheader::Constants::URL_CHARACTERS }
   context 'url characters' do
-    it 'contains lowercase literals'
-    it 'contains uppercase literals'
-    it 'contains numbers'
-  end
-  it 'contains lowercase literals' do
-    ('a'..'z').each do |lowercase|
-      expect(:urL_characters).to include(lowercase)
+    it 'contains lowercase literals' do
+      ('a'..'z').each do |lowercase|
+        expect(url_characters).to include(lowercase)
+      end
+    end
+
+    it 'contains uppercase literals' do
+      ('A'..'Z').each do |uppercase|
+        expect(url_characters).to include(uppercase)
+      end
+    end
+
+    it 'contains numbers' do
+      ('0'..'9').each do |numeral|
+        expect(url_characters).to include(numeral)
+      end
     end
   end
 
-  it 'contains uppercase literals' do
-    ('A'..'Z').each do |uppercase|
-      expect(:urL_characters).to include(uppercase)
-    end
-  end
-
-  it 'contains numbers' do
-    (0..9).each do |numeral|
-      expect(:urL_characters).to include(numeral)
-    end
-  end
 end
