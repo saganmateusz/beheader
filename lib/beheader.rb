5777b7de3620f03071d17371a90be8fcc2ac4cb7
Mateusz Sagan <m.sagan@binarapps.com>
2016-10-09 05:00:51 +0200

    initial commit

---
+++ lib/beheader.rb
@@ -0,0 +1,5 @@
+require "beheader/version"
+
+module Beheader
+  # Your code goes here...
+end
---
+++ spec/beheader_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Beheader do
+  it 'has a version number' do
+    expect(Beheader::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
